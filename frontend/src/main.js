3a9cf715b9b29e6a7c70f6e0e8a1614f3c9785f0
zhangdf <define.zhang@qq.com>
2019-04-09 13:12:07 +0800

    增加自定义CSS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '../static/css/custom.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import BootStrapVue from 'bootstrap-vue'
 // 引入element-UI框架
